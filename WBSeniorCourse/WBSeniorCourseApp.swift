c75841a611d129723d02e3bf53c65a131a22f8da
shutrup <magagaraev95@mail.ru>
2024-07-14 13:41:21 +0300

    Initial Commit

---
+++ WBSeniorCourse/WBSeniorCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSeniorCourseApp.swift
+//  WBSeniorCourse
+//
+//  Created by Шарап Бамматов on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBSeniorCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
